8fae59106e5bd863b1042b820928a4d54548f7a3
khadija-urmi <khadija5jahan211@gmail.com>
2025-05-13 02:22:06 +0600

    user path is updated

--- index.js
+++ index.js
@@ -35,12 +35,25 @@ async function run() {
 
         app.post("/users", async (req, res) => {
             const users = req.body;
+            const query = { email: users.email }
+            const existingUser = await userCollection.findOne(query);
+
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+
             const result = await userCollection.insertOne(users);
             console.log(result);
             res.send(result);
         });
         app.get("/users", async (req, res) => {
             const result = await userCollection.find().toArray();
+            res.send(result);
+        });
+        app.get("/users/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const result = await userCollection.findOne(query);
             res.send(result);
         });
 
@@ -161,6 +174,268 @@ async function run() {
                 res.status(500).json({ message: "Error booking parcel", error: error.message });
             }
         });
+
+        /* ------------------------------------ update parcel ----------------------------------- */
+        app.patch("/parcel/:id", async (req, res) => {
+            const id = req.params.id;
+            const item = req.body;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    name: item.name,
+                    email: item.email,
+                    phone: item.phone,
+                    parcelType: item.parcelType,
+                    receiverName: item.receiverName,
+                    receiverPhone: item.receiverPhone,
+                    deliveryDate: item.deliveryDate,
+                    totalPrice: item.totalPrice,
+                    addressLatitude: item.addressLatitude,
+                    addressLongitude: item.addressLongitude,
+                    deliveryAddress: item.deliveryAddress,
+                    bookingDate: item.bookingDate,
+                    parcelWeight: item.parcelWeight,
+                    // parcelType: item.parcelType
+                },
+            };
+            const result = await parcelCollection.updateOne(filter, updateDoc);
+
+            res.send(result);
+        });
+        /* ------------------------------ delete parcel ----------------------------- */
+        app.delete("/parcel/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const result = await parcelCollection.deleteOne(filter);
+
+            res.send(result);
+        });
+
+
+        app.patch("/updateUser/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            console.log('126', user)
+            const query = { email };
+            const updateDoc = {
+                $set: {
+                    photo: user.photo
+                    ,
+                },
+            };
+            console.log('199------', updateDoc)
+            const result = await userCollection.updateOne(query, updateDoc);
+            console.log('201------', result)
+            // res.send(result);
+        });
+
+        app.patch('/deliveryInfo/:id', async (req, res) => {
+            const id = req.params.id
+            //  console.log(id)
+            const data = req.body
+            // console.log(data)
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    deliveryMan: data.deliveryMan,
+                    deliveryEmail: data.deliveryEmail,
+                    status: data.status,
+                    approximateDate: data.date,
+                    deliveryMenID: data.deliveryMenID
+                }
+            }
+
+            const result = await parcelCollection.updateOne(filter, updateDoc);
+            // console.log(result)
+
+            res.send(result);
+        })
+
+
+        /* -------------------------------- delivery -------------------------------- */
+
+        app.get("/delivery", async (req, res) => {
+            const filter = { role: 'Delivery Man' }
+
+            const result = await userCollection.find(filter).toArray()
+            res.send(result);
+        });
+        app.get('/myDeliveryList/:email', async (req, res) => {
+            const email = req.params.email
+            const filter = { deliveryEmail: email }
+            const result = await parcelCollection.find(filter).toArray()
+            res.send(result);
+        })
+
+        /* -------------------------------- // admin -------------------------------- */
+        app.patch('/changeRole/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'Delivery Man'
+                }
+            }
+
+            const result = await userCollection.updateOne(filter, updateDoc);
+            console.log(result)
+
+            res.send(result);
+        })
+
+        app.patch('/changeAdminRole/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'Admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc);
+            console.log(result)
+
+            res.send(result);
+        })
+
+        app.delete("/deliveryList/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const result = await parcelCollection.deleteOne(filter);
+
+            res.send(result);
+        });
+
+
+        app.patch("/deliveryList", async (req, res) => {
+            const { id, deliveryMenID } = req.body;
+            console.log('id-------->', id)
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    status: 'delivered',
+                },
+
+                $inc: { count: 1 }
+            }
+            const query = { _id: new ObjectId(deliveryMenID) }
+            const updateCount = {
+                // count
+                $inc: { deliveredCount: 1 }
+            }
+            const d = await userCollection.updateOne(query, updateCount);
+            console.log('hjg----------', d)
+            const result = await parcelCollection.updateOne(filter, updateDoc);
+
+            res.send(result);
+        });
+        app.get('/delivered', async (req, res) => {
+            const filter = { status: "delivered" }
+            const result = await parcelCollection.find(filter).toArray()
+            res.send(result);
+        })
+        /* --------------------------------- reviews -------------------------------- */
+        app.post("/reviews", async (req, res) => {
+            const reviews = req.body;
+            console.log('358------->', reviews)
+
+            const result = await reviewCollection.insertOne(reviews);
+            // console.log('137------',result);
+            res.send(result);
+        });
+        app.get("/reviews/:email", async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email }
+
+            const result = await reviewCollection.find(filter).toArray();
+            res.send(result);
+        });
+
+        app.get("/reviews", async (req, res) => {
+            // const email = req.params.email;
+            //  const filter= {deliveryEmail:email}
+
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.get('/topDelivered', async (req, res) => {
+
+            const topDeliveryMen = await reviewCollection.aggregate([
+                {
+                    $group: {
+                        _id: "$email",
+
+                        ratting: {
+                            $avg: "$ratting"
+                        }
+
+                    }
+                },
+                {
+                    $addFields: {
+                        ratting: { $round: ['$ratting', 2] }
+                    }
+                },
+                {
+                    $lookup: {
+                        from: 'users',
+                        localField: '_id',
+                        foreignField: 'email',
+                        as: 'info'
+                    },
+                },
+                { $unwind: '$info' },
+
+                {
+                    $sort: {
+                        "info.deliveredCount": -1,
+                        ratting: -1
+                    }
+                },
+                {
+                    $limit: 3
+                }
+            ]).toArray()
+
+            res.send(topDeliveryMen)
+        })
+
+
+        app.get('/allDelivered', async (req, res) => {
+
+            const topDeliveryMen = await userCollection.aggregate([
+                {
+                    $match: {
+                        role: 'Delivery Man'
+                    }
+                },
+                {
+                    $lookup: {
+                        from: 'reviews',
+                        localField: 'email',
+                        foreignField: 'email',
+                        as: 'info'
+                    },
+                },
+                {
+                    $addFields: {
+                        avgRating: {
+                            $ifNull: [{ $avg: "$info.ratting" }, 0]
+                        }
+                    }
+                },
+                {
+                    $project: {
+                        info: 0
+                    }
+                }
+                // { $unwind: '$info'},
+
+
+            ]).toArray()
+
+            res.send(topDeliveryMen)
+        })
 
     } finally {
 
